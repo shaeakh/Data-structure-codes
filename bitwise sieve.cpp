f32ef68a0909d522ec5698a33081428bae1b78f6
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2023-08-30 15:42:29 +0600

    Add files via upload

---
+++ bitwise sieve.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(int N,int pos){
+    return (bool)(N & (1<<pos));
+}
+int Set(int N,int pos){
+    return N = N | (1<<pos);
+}
+
+int N = 100,prime[100];
+vector<int>status(100/32,0);
+
+void sieve(){
+    int i,j,sqrtN;
+    sqrtN = int ( sqrt(N) );
+    for(int i=3; i<=sqrtN; i += 2){
+        if(check(status[i/32],i&31)==0){
+            for(j=i*i; j<=N; j += 2*i){
+                status[j/32] = Set(status[j/32],j&31);
+            }
+        }
+    }
+    puts("2");
+    for(int i=3; i<=N; i += 2){
+        if(check(status[i/32],i&31)==0) cout<<i<<endl;
+    }
+}
+
+int main(){
+    sieve();
+}
