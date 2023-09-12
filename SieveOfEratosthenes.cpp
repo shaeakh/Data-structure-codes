6606e6fea1493f3d8ab11a8ae5b6b1b833d5b2be
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2023-09-12 22:42:29 +0600

    Update SieveOfEratosthenes.cpp

--- SieveOfEratosthenes.cpp
+++ SieveOfEratosthenes.cpp
@@ -14,21 +14,24 @@ using namespace std;
 #define for_n for(ll i = 0; i < n; i++)
 #define pb push_back
 
-void sieve(int n) 
+vector<ll>prime;
+void sieve(int n)
 {
-    bool prime[n + 1];
-    memset(prime, true, sizeof(prime));
-    
-    //we escaped 2, without 2 all the evens are prime
+    bool isprime[n + 1];
+    memset(isprime, true, sizeof(isprime));
+
+    //we escaped 2, without 2 all the evens are isprime
     //we don't even need to go to the even numbers
+    prime.push_back(2);
     for (int p = 3; p * p <= n; p=p+2) { //thats why odd + 2 = odd ( p = p + 2 )
-        if (prime[p] == true) {
+        if (isprime[p] == true) {
+            prime.push_back(p);
             for (int i = p * p; i <= n; i += 2*p) //thats why odd + 2*odd = odd ( i += 2*p )
-                prime[i] = false;
+                isprime[i] = false;
         }
     }
+    //for(int i=0;i<prime.size();i++){cout<<prime[i]<<" ";}
 
-    
 }
 
 int main(){
