89a29702a8c07491eb2bfdb6f57563b4efab403d
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2023-09-16 10:47:01 +0600

    Update bitwise sieve(more optimized).cpp

--- 3.number theory/Sieve/bitwise sieve(more optimized).cpp
+++ 3.number theory/Sieve/bitwise sieve(more optimized).cpp
@@ -1,32 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool check(int N,int pos){
+#define ll long long int
+
+vector<ll>PrimeList;
+
+bool check(ll N,int pos){
     return (bool)(N & (1<<pos));
 }
-int Set(int N,int pos){
+int Set(ll N,int pos){
     return N = N | (1<<pos);
 }
 
 
 
-void sieve(){
-    int N = 100,prime[100];
-    vector<int>status(100>>5,0);
+void BitWise_sieve(ll N){
+    ll n = (N+32-1)>>5 ;
+    vector<int>status(n,0);
     int i,j,sqrtN;
     sqrtN = int ( sqrt(N) );
-    for(int i=3; i<=sqrtN; i += 2){
+
+    PrimeList.push_back(2);
+    for(int i=3; i<=N; i += 2){
         if(check(status[i>>5],i&31)==0){
+            PrimeList.push_back(i);
             for(j=i*i; j<=N; j += 2*i){
                 status[j>>5] = Set(status[j>>5],j&31);
             }
         }
     }
-    puts("2");
-    for(int i=3; i<=N; i += 2){
-        if(check(status[i>>5],i&31)==0) cout<<i<<endl;
+    
+    for(int i=0; i<PrimeList.size(); i += 1){
+        cout<< PrimeList[i] <<" ";
     }
 }
 
 int main(){
-    sieve();
+    ll n; cin>>n;
+    BitWise_sieve(n);
 }
