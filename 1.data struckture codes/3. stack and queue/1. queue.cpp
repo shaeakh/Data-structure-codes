af1c7c5adbf25023fd441e399e79345eae5c83ec
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-09-23 13:30:39 +0600

    .

---
+++ 1.data struckture codes/3. stack and queue/1. queue.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class QUEUE
+{
+private:
+    vector<int> q;
+    int front = 0, back = 0, size;
+
+public:
+    QUEUE(int size)
+    {
+        q.resize(size);
+        this->size = size;
+    };
+    void push_(int x)
+    {
+        if (back < this->size)
+        {
+            q[back] = x;
+            back++;
+        }
+        else cout<<"Queue is full\n";
+    }
+    void pop_(){
+        if(front<back){
+            front++;
+        }
+        else cout<<"Queue is empty\n";
+    }
+    int getFront(){
+        if(front<=back){
+            return q[front];
+        }        
+        else return -1;
+    }
+    int getBack(){
+        if(front<=back){
+            return q[back-1];
+        }      
+        else return -1;      
+    }
+    
+    bool IsEmpty(){
+        if(front==back) return true;
+        return false;
+    }
+
+};
+
+int main()
+{
+    QUEUE q(5);
+    // cout<<"\n"<<q.IsEmpty()<<endl<<"\n";
+
+    // q.push_(1);
+    // q.push_(2);
+    // q.push_(3);
+    // q.push_(4);
+    
+    // cout<<"\n"<<q.IsEmpty()<<endl<<"\n";
+    // cout<<"\n"<<q.getFront()<<endl<<"\n";
+    // cout<<"\n"<<q.getBack()<<endl<<"\n";
+    // q.pop_();
+    // cout<<"\n"<<q.getFront()<<endl<<"\n";
+
+
+
+}
---
+++ 1.data struckture codes/3. stack and queue/try.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*bismillahir~rahmanir~rahim*/
+
+class STACK
+{
+private:
+    int top = -1, size;
+    vector<int> s;
+public:
+    STACK(int size){
+        this->size = size;
+        s.resize(size);
+    };
+
+    void push_(int x){
+        if(top<size){
+            top++;
+            s[top] = x;
+        }
+        else cout<<"Stack is full\n";
+    }
+
+    void pop_(){
+        if(top>=0){
+            top--;
+        }
+        else cout<<"Stack is empty\n";
+    }
+
+    int getTop(){
+        if(top>=0){
+            return s[top];
+        }
+        else return -1;
+    }
+    
+    bool IsEmpty(){
+        if(top==-1) return true;
+        else return false;
+    }
+    
+};
+
+
+
+int main()
+{
+    STACK c(5);
+
+    // cout<<"\n"<<c.IsEmpty()<<endl<<"\n";
+
+    // c.push_(1);
+    // c.push_(2);
+    // c.push_(3);
+    // c.push_(4);
+    
+    // // cout<<"\n"<<c.IsEmpty()<<endl<<"\n";
+    // // cout<<"\n"<<c.getTop()<<endl<<"\n";    
+    // c.pop_();
+    // cout<<"\n"<<c.getTop()<<endl<<"\n";   
+
+
+
+
+}
+/* problem link: */
